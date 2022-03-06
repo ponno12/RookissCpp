c81b425839896e66df820a499b67ffa6d87a5365
ponno12 <ponno145@gmail.com>
2022-03-06 18:52:43 +0900

    초기 커밋.

---
+++ CharNString.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+//문자와 문자열
+
+//char : 알파벳 / 숫자 문자를 나타냄
+//wchar_t : 유니코드 문자를 나타낸다.
+
+// ASCII (미국표준 코드)
+char ch = 97; // 1바이트
+char ch2 = '1'; // 1바이트
+char ch3 = 'a'+1; // 1바이트
+
+
+//국제화 시대에는 영어만으로 서비스할수 없음으로 대체하는게 유니코드
+//중국어와 한국어가 제일 많은 번호를 차지함
+//표기 방식이 여러가지가 있음 utf-8/ utf-16
+
+//utf-8 알파벳 , 숫자 1바이트
+//utf-16 유럽지역 문자는 2바이트
+//wchar_t 는 유니코드를 저장한다
+wchar_t wch = L'노';
+int main()
+{    
+	cout << ch << endl << ch2 << ch3 << endl;
+	//setlocale(LC_ALL, "");
+	wcout.imbue(locale("korean"));
+	wcout << wch << endl;
+
+}
+
---
+++ Integer.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+
+int hp = 100;
+
+char a;  // 1바이트 (-128~127)
+short b; // 2바이트 (-32786~+32767)
+int c;   // 4바이트(-21.4억 ~21.4억)
+__int64 d ; // 8바이트 (long long) 어마어마하게 큰단위
+
+//양수만 비트 앞에서 부호비트를 없애줌
+unsigned char ua;  // 1바이트 (0~255)
+unsigned short ub; // 2바이트 (0~65536)
+unsigned int uc;   // 4바이트(0~42.9억)
+unsigned __int64 ud; // 8바이트 (long long) 어마어마하게 큰단위
+
+
+// 귀찮은 그냥 대충 4바이트로 가면 안될까?
+
+int main()
+{
+    cout << "체력이 " <<hp << " 남았습니다" << endl;
+    
+}
+
